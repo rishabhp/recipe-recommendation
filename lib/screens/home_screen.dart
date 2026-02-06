2166bf343854f8f15c2ca4116308cf6d708120a7
rishabh <rishabhp0608@gmail.com>
2026-02-06 17:33:33 +0530

    Fixed versions of dependencies to avoid conflict

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Welcome to our website! $AuthService().currentUser")));
+    return Scaffold(body: Center(child: Text("Welcome to our website! ${AuthService().currentUser!.email}")));
   }
 }
